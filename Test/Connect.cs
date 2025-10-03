41566b6f09be95b8ddd224f96d15503d0e71dc52
FZoltan38 <farkaszoltan28@gmail.com>
2025-10-03 10:37:04 +0200

    Connect class is ready to use..

--- Test/Connect.cs
+++ Test/Connect.cs
@@ -1,6 +1,28 @@
-﻿namespace Test
+﻿using MySql.Data.MySqlClient;
+
+namespace Test
 {
     public class Connect
     {
+        public MySqlConnection? Connection;
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string ConnectioString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "library";
+            _user = "root";
+            _password = "";
+
+            ConnectioString = $"SERVER={_host}; DATABSASE={_db};UID={_user};PASSWORD={_password};SslMode=none";
+
+            Connection = new MySqlConnection(ConnectioString);
+
+        }
     }
 }
